6aabf7e5834d22f79f3bf07586a1d59496b1a65a
hetchzhao <hetchzhao@aliyun.com>
2022-01-13 14:17:37 +0800

    add observer

---
+++ src/observer/ConcreteSubject.ts
@@ -0,0 +1,42 @@
+import IObserver from "./IObserver";
+import ISubject from "./ISubject";
+
+export default class ConcreteSubject implements ISubject {
+  private observers: IObserver[] = [];
+  public state!: number;
+
+  public attach(observer: IObserver) {
+    const isExist = this.observers.includes(observer);
+    if (isExist) {
+      return console.log("Subject: Observer has been attached already.");
+    }
+
+    console.log("Subject: Attached an observer.");
+    this.observers.push(observer);
+  }
+
+  public detach(observer: IObserver) {
+    const observerIndex = this.observers.indexOf(observer);
+    if (observerIndex === -1) {
+      return console.log("Subject: Nonexistent observer.");
+    }
+
+    this.observers.splice(observerIndex, 1);
+    console.log("Subject: Detached an observer.");
+  }
+
+  public notify() {
+    console.log("Subject: Notifying observers...");
+    for (const observer of this.observers) {
+      observer.update(this);
+    }
+  }
+
+  public someBusinessLogic(): void {
+    console.log("\nSubject: I'm doing something important.");
+    this.state = Math.floor(Math.random() * (10 + 1));
+
+    console.log(`Subject: My state has just changed to: ${this.state}`);
+    this.notify();
+  }
+}
---
+++ src/observer/ISubject.ts
@@ -0,0 +1,9 @@
+import IObserver from "./IObserver";
+
+export default interface ISubject {
+  attach(observer: IObserver): void;
+
+  detach(observer: IObserver): void;
+
+  notify(): void;
+}
